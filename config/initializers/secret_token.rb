0d7e787b4a89c5ac984b3edffcbfe0e26621de97
U-FISERVISD\ssuh <ssuh@ssuh3-lt.FiservISD.FiservISDRoot.local>
2011-07-11 20:17:02 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '774068004cbbe2938f475e59dc9cc7f384a771d8b52dc757b057d0f959752ceaba075cd8ec28fa1a3e86a4f4bcf0b82eb751e7bbc25dec820dc1e8c88fedf862'
